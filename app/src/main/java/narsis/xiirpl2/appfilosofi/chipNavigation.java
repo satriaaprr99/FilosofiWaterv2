b5c8a76b75734b58d0962f98776633009341dba1
sahlanputrap <sahlan.pratama.6@gmail.com>
2021-03-01 11:07:41 +0700

    fragment

--- app/src/main/java/narsis/xiirpl2/appfilosofi/chipNavigation.java
+++ app/src/main/java/narsis/xiirpl2/appfilosofi/chipNavigation.java
@@ -1,14 +1,50 @@
 package narsis.xiirpl2.appfilosofi;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
 public class chipNavigation extends AppCompatActivity {
+
+    ChipNavigationBar chipNavigationBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_chip_navigation);
+
+        chipNavigationBar = findViewById(R.id.menu_bottom);
+        bottomMenu();
+    }
+    private void bottomMenu(){
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                Fragment fragment = null;
+                switch (i){
+                    case R.id.homepage:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.feed:
+                        fragment = new FeedFragment();
+                        break;
+                    case R.id.shop:
+                        fragment = new ShopFragment();
+                        break;
+                    case R.id.notify:
+                        fragment = new NotifyFragment();
+                        break;
+                    case R.id.profile:
+                        fragment = new ProfileFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+            }
+        });
     }
 }
