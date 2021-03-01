1d1aa45e0fb04a8634b6a67d27c473176fe5fa7d
satriaaprr99 <61263829+satriaaprr99@users.noreply.github.com>
2021-03-01 11:57:09 +0700

    revisi 4

--- app/src/main/java/narsis/xiirpl2/appfilosofi/HomeFragment.java
+++ app/src/main/java/narsis/xiirpl2/appfilosofi/HomeFragment.java
@@ -1,18 +1,34 @@
 package narsis.xiirpl2.appfilosofi;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class HomeFragment extends Fragment {
+    Button btnshop;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+     btnshop = root.findViewById(R.id.btnshop);
+     btnshop.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+             Intent intent = new Intent(getActivity().getApplication(), MainActivity.class);
+             startActivity(intent);
+         }
+     });
+
+     return root;
     }
 }
---
+++ app/src/main/java/narsis/xiirpl2/appfilosofi/MainHome.java
@@ -0,0 +1,7 @@
+package narsis.xiirpl2.appfilosofi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainHome extends AppCompatActivity {
+
+}
