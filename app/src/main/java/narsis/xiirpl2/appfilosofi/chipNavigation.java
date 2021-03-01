3ec29fc0329e99fa99fec29d4bc1857302020781
sahlanputrap <sahlan.pratama.6@gmail.com>
2021-03-01 10:27:20 +0700

    home

---
+++ app/src/main/java/narsis/xiirpl2/appfilosofi/chipNavigation.java
@@ -0,0 +1,14 @@
+package narsis.xiirpl2.appfilosofi;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class chipNavigation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chip_navigation);
+    }
+}
---
+++ app/src/main/java/narsis/xiirpl2/appfilosofi/login.java
@@ -0,0 +1,28 @@
+package narsis.xiirpl2.appfilosofi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class login extends AppCompatActivity {
+
+    Button login_butt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        login_butt = findViewById(R.id.buttlog);
+        login_butt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(login.this, chipNavigation.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
