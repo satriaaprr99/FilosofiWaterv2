66c84500b9ce6e4d098a9d7f10aaa9c84fcfb573
sahlanputrap <sahlan.pratama.6@gmail.com>
2021-03-06 12:16:29 +0700

    layout2

--- app/src/main/java/narsis/xiirpl2/appfilosofi/api/FilosofiServer.java
+++ app/src/main/java/narsis/xiirpl2/appfilosofi/api/FilosofiServer.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class FilosofiServer {
-    private static final String base_url = "http://172.16.100.220:8000/api/";
+    private static final String base_url = "http://192.168.1.4:8000/api/";
     private static Retrofit retrofit;
 
     public static Retrofit getClient(){
