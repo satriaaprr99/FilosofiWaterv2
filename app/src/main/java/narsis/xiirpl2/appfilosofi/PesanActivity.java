a6f6800987e0ea12e108a792db5762ef0295b776
satriaaprr99 <61263829+satriaaprr99@users.noreply.github.com>
2021-03-08 09:52:10 +0700

    pesan

---
+++ app/src/main/java/narsis/xiirpl2/appfilosofi/PesanActivity.java
@@ -0,0 +1,11 @@
+package narsis.xiirpl2.appfilosofi;
+
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PesanActivity extends AppCompatActivity {
+    TextView nama,jenis,stok,harga;
+
+
+}
